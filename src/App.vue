5f7ed64904f7988f85246bae033d6f752147b7b2
KirillM <akkforsilver@yandex.ru>
2022-10-30 17:25:49 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  data() {
+    return {
+      likes: 0,
+    }
+  }
+})
+</script>
+
+<template>
+  <div>{{ likes }}</div>
+</template>
+
+
