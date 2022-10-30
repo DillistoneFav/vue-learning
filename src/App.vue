5bd87855b3a76a67ac7172eb81503ac8f624608f
KirillM <akkforsilver@yandex.ru>
2022-10-30 22:36:17 +0300

    it seems like the correct counter

--- src/App.vue
+++ src/App.vue
@@ -6,31 +6,41 @@ export default defineComponent({
   name: "App",
   components: {},
   data() {
+    let likes: number = 0
     return {
-      likes: 0,
-      dislikes: 5,
+      likes,
     }
   },
   methods: {
     addLike() {
       this.likes += 1
     },
-    addDislike() {
-      this.dislikes += 1
-    }
   }
 })
 </script>
 
 <template>
-  <div>
+  <div class="app">
     <button @click="addLike">add like</button>
-    <button @click="addDislike">add dislike</button>
-  </div>
-  <div>
-    likes: {{ likes }}<br/>
-    dislikes: {{ dislikes }}
+    <div>{{likes}}</div>
   </div>
 </template>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  .app{
+    width: 100%;
+    height: 100vh;
+    background: #000;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+</style>
 
 
