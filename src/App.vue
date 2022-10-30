c5a99099d942d7368c6efd641e5dcf91098fe717
KirillM <akkforsilver@yandex.ru>
2022-10-30 17:37:03 +0300

    just counters

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,29 @@ export default defineComponent({
   data() {
     return {
       likes: 0,
+      dislikes: 5,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1
+    },
+    addDislike() {
+      this.dislikes += 1
     }
   }
 })
 </script>
 
 <template>
-  <div>{{ likes }}</div>
+  <div>
+    <button @click="addLike">add like</button>
+    <button @click="addDislike">add dislike</button>
+  </div>
+  <div>
+    likes: {{ likes }}<br/>
+    dislikes: {{ dislikes }}
+  </div>
 </template>
 
 
